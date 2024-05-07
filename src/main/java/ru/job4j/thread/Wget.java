d3c9ce286324ac5fe8c369c5b8e96671a50978fe
arganaft <e1mar.arganaft@gmail.com>
2024-05-07 18:46:44 +0300

    3.1.1. Threads 8. Скачивание файла с ограничением.

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -9,33 +9,41 @@ import java.nio.file.Files;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String tempFile;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String tempFile) {
         this.url = url;
         this.speed = speed;
+        this.tempFile = tempFile;
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        var file = new File("tmp.xml");
+        var file = new File(tempFile);
         try (var input = new URL(url).openStream();
              var output = new FileOutputStream(file)) {
+            long startDowload = System.currentTimeMillis();
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
             var dataBuffer = new byte[512];
             int bytesRead;
+            int dowloadByte = 0;
+            var downloadAt = System.currentTimeMillis();
             while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
                 output.write(dataBuffer, 0, bytesRead);
-                long dowloadTimeNano = System.nanoTime() - downloadAt;
-                System.out.println("Read 512 bytes : " + dowloadTimeNano + " nano.");
-                if (speed != 6000) {
-                    long calculateSleep = calculateSleep(dowloadTimeNano);
-                    System.out.printf("Thread.sleep(%d)%n", calculateSleep);
-                    Thread.sleep(calculateSleep);
+                dowloadByte += bytesRead;
+                if (dowloadByte >= speed) {
+                    long dowloadTimeMillis = System.currentTimeMillis() - downloadAt;
+                    System.out.printf("Dowload %d Byte in: %d Millis%n", dowloadByte, dowloadTimeMillis);
+                    long sleepTime = (1000 - dowloadTimeMillis) * (dowloadByte / speed);
+                    System.out.printf("Thread.sleep(%d)%n", sleepTime);
+                    Thread.sleep(sleepTime);
+                    dowloadByte = 0;
+                    downloadAt = System.currentTimeMillis();
                 }
-
             }
+            long finish = System.currentTimeMillis() - startDowload;
+            System.out.printf("Finish in %d%n", finish);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -48,23 +56,43 @@ public class Wget implements Runnable {
         }
     }
 
-    private long calculateSleep(long dowloadTimeNano) {
-        return (512 * (1000000 / dowloadTimeNano)) / speed;
-    }
 
     public static void main(String[] args) throws InterruptedException {
-        if (validate(args)) {
-            String url = args[0];
-            int speed = Integer.parseInt(args[1]);
-            Thread wget = new Thread(new Wget(url, speed));
-            wget.start();
-            wget.join();
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Не определены аргументы");
         }
+        int urlIndex = validateURL(args);
+        int speedIndex = validateSpeed(args);
+        if (urlIndex == -1 && speedIndex == -1) {
+            throw new IllegalArgumentException("Переданные аргументы не содержат URL файла(которое нужно загрузить) и число(скорость загрузки)");
+        }
+        String url = args[urlIndex];
+        int speed = Integer.parseInt(args[speedIndex]);
+        Thread wget = new Thread(new Wget(url, speed, "tmp.txt"));
+        wget.start();
+        wget.join();
 
     }
 
-    private static boolean validate(String[] args) {
-        return (args[0].startsWith("https://") || args[0].startsWith("http://"))
-                && args.length >= 2 && args[1].matches("-?\\d+");
+    private static int validateURL(String[] args) {
+        int urlIndex = -1;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("https://") || args[i].startsWith("http://")) {
+                urlIndex = i;
+                break;
+            }
+        }
+        return urlIndex;
+    }
+
+    private static int validateSpeed(String[] args) {
+        int speedIndex = -1;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].matches("-?\\d+")) {
+                speedIndex = i;
+                break;
+            }
+        }
+        return speedIndex;
     }
 }
